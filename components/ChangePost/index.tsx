d253a3d43686403cbe3ee8525a91c32d600edc22
svyatoslav <svyatoslavtt>
2021-01-27 17:39:10 +0200

    Add close modal button, isFieldsMatch

--- components/ChangePost/index.tsx
+++ components/ChangePost/index.tsx
@@ -21,11 +21,36 @@ const Container = styled.section`
 `;
 
 const FormWrapper = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: flex-end;
+	position: relative;
 	width: 100%;
 	max-width: 600px;
 	padding: 70px 50px 50px;
 	background: #fff;
 	border-radius: 5px;
+`;
+
+const Button = styled.button`
+	position: absolute;
+	top: 20px;
+	right: 20px;
+	height: 40px;
+	width: 40px;
+	background-color: #f7f7f7;
+	color: #333333;
+	font-size: 15px;
+	border-radius: 50%;
+	border: 1px solid #333333;
+	cursor: pointer;
+	outline: none;
+	transition: all .1s ease-in-out;
+
+	&:hover {
+		color: #f7f7f7;
+		background-color: #5a5a5a;
+	}
 `;
 
 type ChangePostTypes = {
@@ -37,16 +62,28 @@ const ChangePost: React.FC<ChangePostTypes> = ({ setIsChangePost }) => {
 	const currentPost = useSelector(getCurrentPost);
 
 	const handleSubmit = (event: FormEvent, params: FormDataTypes) => {
+		event.preventDefault();
+
 		const { router, ...rest } = params;
 
-		event.preventDefault();
-		dispatch(Actions.updatePostRequest({ ...rest, id: currentPost.id }));
+		const currentPostData = {
+			title: currentPost.title,
+			body: currentPost.body
+		};
+	
+		const isFieldsMatch = Object.keys(currentPostData).some((item: string) => currentPostData[item] !== rest[item]);
+
+		if (isFieldsMatch) {
+			dispatch(Actions.updatePostRequest({ ...rest, id: currentPost.id }));
+		};
+
 		setIsChangePost(false);
 	};
 	
 	return (
 		<Container>
 			<FormWrapper>
+				<Button onClick={setIsChangePost.bind(null, false)}>X</Button>
 				<PostForm currentPost={currentPost} handleSubmit={handleSubmit} />
 			</FormWrapper>
 		</Container>
