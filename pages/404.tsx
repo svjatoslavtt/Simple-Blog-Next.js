7d793259aec5b8aa84dc4b5aa49258f71fe3acff
svyatoslav <svyatoslavtt>
2021-01-28 09:51:04 +0200

    Add 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+const Container = styled.section`
+  with: 100%;
+  text-align: center;
+`;
+
+const Custom404 = () => {
+  return (
+    <Container>
+      <h1>404 - Page Not Found</h1>
+    </Container>
+  );
+};
+
+export default Custom404;
