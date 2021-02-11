1626d61fe2f387a82ddc81591d6065bbea572c62
svyatoslav <svyatoslavtt>
2021-02-11 13:01:19 +0200

    Refactor selectors

--- redux/posts/reducer.ts
+++ redux/posts/reducer.ts
@@ -7,56 +7,63 @@ import { ActionTypes, ActionTypeUnion } from './actions';
 import { Comment, Post } from '../../types/post.types';
 
 export type PostsState = {
-	posts: Post[];
-	currentPost: Post;
-	comments: Comment[];
+  posts: Post[];
+  currentPost: Post;
+  comments: Comment[];
 };
 
 const postsInitialState: PostsState = {
-	posts: null,
-	currentPost: null,
-	comments: null,
+  posts: null,
+  currentPost: null,
+  comments: null,
 };
 
 export const postsReducer = (
-	state = postsInitialState,
-	action: ActionType<ActionTypeUnion | AnyAction>
+  state = postsInitialState,
+  action: ActionType<ActionTypeUnion | AnyAction>
 ) => {
-	switch (action.type) {
-		case HYDRATE:
-			return {
-				...state,
-				...action.payload,
-			};
-		case ActionTypes.GET_POSTS_SUCCESS:
-			return {
-				...state,
-				posts: action.payload,
-			};
-		case ActionTypes.GET_CURRENT_POST_SUCCESS:
-			const { comments, ...rest } = action.payload;
+  switch (action.type) {
+    case HYDRATE:
+      return {
+        ...state,
+        ...action.payload,
+      };
+    case ActionTypes.GET_POSTS_SUCCESS:
+      return {
+        ...state,
+        posts: action.payload.sort((a: Post, b: Post) => b.id - a.id),
+      };
+    case ActionTypes.GET_CURRENT_POST_SUCCESS:
+      const { comments, ...rest } = action.payload;
+      const filteredComments = comments
+        .filter((item: Comment) => item.postId === rest.id)
+        .sort((a: Comment, b: Comment) => Number(b.id) - Number(a.id));
 
-			return {
-				...state,
-				currentPost: rest,
-				comments,
-			};
-		case ActionTypes.ADD_COMMENT_SUCCESS:
-			return {
-				...state,
-				comments: [...state.comments, action.payload],
-			};
-		case ActionTypes.ADD_NEW_BLOG_SUCCESS:
-			return {
-				...state,
-				posts: [...state.posts, action.payload],
-			};
-		case ActionTypes.UPDATE_POST_SUCCESS:
-			return {
-				...state,
-				currentPost: action.payload,
-			};
-		default:
-			return state;
-	}
+      return {
+        ...state,
+        currentPost: rest,
+        comments: filteredComments,
+      };
+    case ActionTypes.ADD_COMMENT_SUCCESS:
+      const sortedComments = [...state.comments, action.payload].sort(
+        (a: Comment, b: Comment) => Number(b.id) - Number(a.id)
+      );
+
+      return {
+        ...state,
+        comments: sortedComments,
+      };
+    case ActionTypes.ADD_NEW_BLOG_SUCCESS:
+      return {
+        ...state,
+        posts: [...state.posts, action.payload],
+      };
+    case ActionTypes.UPDATE_POST_SUCCESS:
+      return {
+        ...state,
+        currentPost: action.payload,
+      };
+    default:
+      return state;
+  }
 };
--- redux/posts/selectors.ts
+++ redux/posts/selectors.ts
@@ -2,24 +2,19 @@ import { createSelector } from 'reselect';
 
 import { PostsState } from './reducer';
 
-import { Comment, Post } from '../../types/post.types';
-
 const postsState = (state: PostsState) => state;
 
 export const getPosts = createSelector(
-	postsState,
-	(state: PostsState) =>
-		state.posts && state.posts.sort((a: Post, b: Post) => b.id - a.id)
+  postsState,
+  (state: PostsState) => state.posts
 );
 
 export const getCurrentPost = createSelector(
-	postsState,
-	(state: PostsState) => state.currentPost
+  postsState,
+  (state: PostsState) => state.currentPost
 );
 
 export const getComments = createSelector(
-	postsState,
-	(state: PostsState) =>
-		state.comments &&
-		state.comments.sort((a: Comment, b: Comment) => Number(b.id) - Number(a.id))
+  postsState,
+  (state: PostsState) => state.comments
 );
